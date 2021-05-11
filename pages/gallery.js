17f203bc4d6f2c1c2fb73ce4f59b4904d2dae8b6
ingeborgwa <ingeborg.austad@gmail.com>
2021-05-11 23:43:02 +0200

    created crads for gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import Cosmic from 'cosmicjs';
-
 import styles from '../styles/Home.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltRight  } from '@fortawesome/free-solid-svg-icons';
+
+import PageTitle from '../components/StyledComponents/PageTitle';
+import NavigationBar from '../components/NavigationBar/';
+import Footer from '../components/Footer';
+import { MainContainer } from '../components/StyledComponents/Containers';
+
+
 
 function Gallery () {
     const [artWorks, setArtWorks] = useState([]);
@@ -43,40 +53,38 @@ function Gallery () {
 
     function renderPage() {
         return (
-            <main className={styles.main}>
-                <h1>Galleri</h1>
-                {/* <h3>Akvarell</h3> */}
-                <ul>
-                    {artWorks.map(artWork => {
-                        const image = artWork.metafields.find(metafield => metafield.key === 'main_image')
-                        const height = artWork.metafields.find(metafield => metafield.key === 'height')
-                        const width = artWork.metafields.find(metafield => metafield.key === 'width')
+            <>
+                <NavigationBar/>
+                <main>
+                    <PageTitle>Galleri</PageTitle>
+                    <GalleryGrid>
+                        {artWorks.map(artWork => {
+                            const image = artWork.metafields.find(metafield => metafield.key === 'main_image')
+                            const height = artWork.metafields.find(metafield => metafield.key === 'height')
+                            const width = artWork.metafields.find(metafield => metafield.key === 'width')
+                            
                         
-                    
-                        return (
-                            <li key={artWork.slug}>
-                                {artWork.title}
-                                <img src={image.imgix_url} width="200"/>
-                                <p>{height.value} x {width.value}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-
-
-                {/* <ul>
-                    {artWorks.map(item => {
-                        const image = item.metafields.find(metafield => metafield.key === 'main_image')
-                        return(
-                            <li key={item.slug}>
-                                {item.title}
-                                <img src={image.imgix_url} width="200"/>
-                                
-                            </li>
-                        )
-                    })}
-                </ul> */}
-            </main>
+                            return (
+                                <Card key={artWork.slug}>
+                                    <img 
+                                        src={image.imgix_url} 
+                                        width="200"
+                                    />
+                                    <h6>{artWork.title}</h6>
+                                    <p>{height.value}cm x {width.value}cm</p>
+                                    <Button>
+                                        <p>Se mer</p>
+                                        <FontAwesomeIcon 
+                                            icon={faLongArrowAltRight}
+                                        />
+                                    </Button>
+                                </Card>
+                            )
+                        })}
+                    </GalleryGrid> 
+                </main>
+                <Footer/>
+            </>
         )
     }
 
@@ -85,8 +93,89 @@ function Gallery () {
             {(artWorks.length === 0) ? renderSkeleton() : renderPage()}
         </>
     );
-
-
 }
 
 export default Gallery;
+
+
+
+
+//--------- STYLES ---------//
+
+
+const GalleryGrid = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    max-width: 90%;
+    margin-top: 3rem;
+
+    @media (max-width: 600px) {
+        width: 100%;
+        flex-direction: column;
+    }
+
+`;
+
+
+const Card = styled.article`
+    margin: 1rem;
+    padding: 1.5rem;
+    text-align: left;
+    text-decoration: none;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 10px;
+    transition: color 0.15s ease, border-color 0.15s ease;
+    width: 15em;
+    height: 25em;
+    box-sizing: border-box;
+
+    h6{
+        margin: 0 0 1rem 0;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+
+    p{
+        margin: 0;
+        font-size: 0.8rem;
+        line-height: 1.5;
+    }
+
+
+    img{
+        border-radius: 10px;
+    }
+`;
+
+const Button = styled.button`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    border: none;
+    background-color: white;
+    text-align: left;
+    
+    p{
+        font-size: 0.9rem;
+        padding-right: 0.5em;
+    }
+
+`;
+
+
+
+
+{/* <ul>
+    {artWorks.map(item => {
+        const image = item.metafields.find(metafield => metafield.key === 'main_image')
+        return(
+            <li key={item.slug}>
+                {item.title}
+                <img src={image.imgix_url} width="200"/>
+                
+            </li>
+        )
+    })}
+</ul> */}
